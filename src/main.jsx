c97a2d3cb2840d69f50d3a94a487b43118709e65
Lukas <lukaskvell@outlook.com>
2026-03-11 12:05:49 +0100

    turned root into a variable and wrote some comments for myself

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// 1. Create root
+// 2. Render markup
+const root = createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
+    <h1>PDWaDWA</h1>
     <App />
   </StrictMode>,
-)
+);
